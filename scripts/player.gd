f39b34db9f92ad0c3032d64b3f102dc4bf374d3c
BitUniverse <64bituniverse@gmail.com>
2025-12-19 13:11:00 -0500

    Fixed bullet spawn weirdness from rotating, starting onto coin interactions

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,17 @@
 extends CharacterBody2D
 
 const IS_PLAYER = true
+
 @onready var _character = $Character
-#Originally 'rotation', but apparently that's reserved
-#This is just to store Character Rotation for bullets and things
-@onready var CRotate: String = ""
+@onready var MuzzleVec2: Vector2 = Vector2()
+
+# GLOBAL REFERENCE (global_position). DO NOT COMPARE TO SCENE.
+@onready var MuzzleOffset: Vector2 = Vector2()
+
+@onready var MuzzleSprite: AnimatedSprite2D = $MuzzleSprite
 @onready var RotationTrack: Node2D = $RotationTrack
 @onready var MuzzleSpawn: Marker2D = $RotationTrack/MuzzleSpawn
-@onready var MuzzleSprite: AnimatedSprite2D = $RotationTrack/MuzzleSprite
+
 var bullet_default = preload("res://scenes/bullet.tscn")
 var speed = 100
 
@@ -19,40 +23,50 @@ func get_input():
 		shoot()
 
 func _process(delta):
-
 	var mouse = get_global_mouse_position() - position
 	#If animations are glitching, add advance(0) after .play
 	#It's an issue with the timing of AnimationPlayer getting called.
 	if mouse.angle() < 7*PI/8 && mouse.angle() > 5*PI/8:
 		_character.play("FrontLeft")
-		CRotate = "FrontLeft"
+		MuzzleVec2 = Vector2(-8, 10)
+		MuzzleOffset = Vector2(0,12)
 	elif mouse.angle() <= 5*PI/8 && mouse.angle() >= 3*PI/8:
 		_character.play("Front")
-		CRotate = "Front"
+		MuzzleVec2 = Vector2(3, 11)
+		MuzzleOffset = Vector2(10,7)
 	elif mouse.angle() < 3*PI/8 && mouse.angle() > PI/8:
 		_character.play("FrontRight")
-		CRotate = "FrontRight"
+		MuzzleVec2 = Vector2(8, 10)
+		MuzzleOffset = Vector2(8,0)
 	elif mouse.angle() <= PI/8 && mouse.angle() >= -PI/8:
 		_character.play("Right")
-		CRotate = "Right"
+		MuzzleVec2 = Vector2(14,6)
+		MuzzleOffset = Vector2(0,0)
 	elif mouse.angle() < -PI/8 && mouse.angle() > -3*PI/8:
 		_character.play("BackRight")
-		CRotate = "BackRight"
+		MuzzleVec2 = Vector2(12, -2)
+		MuzzleOffset = Vector2(0,0)
 	elif mouse.angle() <= -3*PI/8 && mouse.angle() >= -5*PI/8:
 		_character.play("Back")
-		CRotate = "Back"
+		MuzzleVec2 = Vector2(0, -19)
+		MuzzleOffset = Vector2(-5,-5)
 	elif mouse.angle() < -5*PI/8 && mouse.angle() > -7*PI/8:
 		_character.play("BackLeft")
-		CRotate = "BackLeft"
+		MuzzleVec2 = Vector2(-12, -2)
+		MuzzleOffset = Vector2(-15,0)
 	elif abs(mouse.angle()) >= 7*PI/8:
 		_character.play("Left")
-		CRotate = "Left"
+		MuzzleVec2 = Vector2(-14, 6)
+		MuzzleOffset = Vector2(-2,12)
 func shoot():
 	if true:
 		var new_bullet = bullet_default.instantiate()
-		new_bullet.global_position = RotationTrack.global_position
-		new_bullet.global_rotation = RotationTrack.global_rotation
+		print(MuzzleSpawn.global_position)
+		var MuzzleSpawnP = MuzzleSpawn.global_position + MuzzleOffset
+		new_bullet.global_position = MuzzleSpawnP
+		new_bullet.global_rotation = MuzzleSpawn.global_rotation
 		# Bullet speed in bullet.gd
+		MuzzleSprite.position = MuzzleVec2
 		MuzzleSprite.play("default")
 		get_parent().add_child(new_bullet)
 	#FUTURE
@@ -67,6 +81,6 @@ func shoot():
 
 func _physics_process(delta):
 	RotationTrack.rotation = lerp_angle(RotationTrack.rotation, (get_global_mouse_position() - global_position).angle(), 6.5*delta)
-	MuzzleSprite.position = Vector2().rotated(-RotationTrack.rotation)
+
 	get_input()
 	move_and_slide()
